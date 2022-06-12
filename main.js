870cc294f3fa1c17505cf948d96c209460076a2d
Amir <107354442+aamir2002@users.noreply.github.com>
2022-06-12 20:30:58 +0600

    Commit #1

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+   const list = document.querySelectorAll('.list');
+   function activeLink(){
+   list.forEach((item) =>
+   item.classList.remove('active'));
+   this.classList.add('active')
+   }
+   list.forEach((item) =>
+   item.addEventListener('click', activeLink));
+
+})
